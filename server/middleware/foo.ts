319a1314e3537b77fd70fe32867a768fa3998e13
Josh Deltener <hecktarzuli@gmail.com>
2023-05-29 21:34:28 -0500

    testing isError

--- server/middleware/foo.ts
+++ server/middleware/foo.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler((event) => {
     const urlObj = getRequestURL(event) 
     console.log(urlObj)
-    
+    console.log(isError(event))
     // https://www.jsdocs.io/package/h3#getRequestURL
   
   })
